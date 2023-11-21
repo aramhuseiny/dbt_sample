bb764a79e8f970e76738901676ed6ff2f0a67f17
Aram <huseiny.aram@gmail.com>
2023-11-21 14:05:22 +0330

    add base code

---
+++ models/staging/parnian/customers.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='table') }}
+
+With
+    customers
+    as
+    (
+        select
+            customerid as customer_id, secondcode as customer_second_code, partyref as party_ref, "Type" as customer_type, "Number" as customer_number
+        from {{ source('parnian', 'customer')}}
+)
+
+select *
+from customers
